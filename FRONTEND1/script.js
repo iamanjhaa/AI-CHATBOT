737578254046cff61a0905b5ed402416551416c9
Aman Kumar <amanjha411@gmail.com>
2026-09-04 14:10:57 +0530

    Connect frontend to deployed backend

--- FRONTEND1/script.js
+++ FRONTEND1/script.js
@@ -1,4 +1,7 @@
-const API_URL = "http://127.0.0.1:8000/chat";
+// const API_URL = "http://127.0.0.1:8000/chat";
+
+
+const API_URL = "https://sahayak-backend-y4f0.onrender.com/chat";
 
 const chatForm = document.getElementById("chatForm");
 const problemInput = document.getElementById("problemInput");
